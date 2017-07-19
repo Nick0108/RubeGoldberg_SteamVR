218f8cf6f57ec0b29d359d2a838760d61075c0c7
Nick0108 <zidonglin@outlook.com>
2017-07-19 09:30:50 +0800

    Feat: Add the foundational Object

---
+++ Assets/Scripts/FunBlow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunBlow : MonoBehaviour {
+
+    public float BlowForce = 50.0f;
+    public Transform FunBlade;
+
+    private void OnTriggerStay(Collider other)
+    {
+        FunBlade.Rotate(0, 0, BlowForce/5.0f);
+        float BlowDistance = Vector3.Distance(gameObject.transform.position, other.transform.position);
+        other.attachedRigidbody.AddForce(gameObject.transform.forward.normalized*BlowForce*(-1.0f) / (BlowDistance * BlowDistance));
+        //other.attachedRigidbody.AddForce(gameObject.transform.forward.normalized * -50f / Mathf.Sqrt(BlowDistance));
+    }
+}
---
+++ Assets/Scripts/TrampolineResilience.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineResilience : MonoBehaviour {
+    public float resilience = 50.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        float v = other.attachedRigidbody.velocity.magnitude;
+        if (v > 10.0f)
+            v = 10.0f;
+        other.attachedRigidbody.AddForce(gameObject.transform.forward * resilience * v, ForceMode.Force);
+    }
+}
