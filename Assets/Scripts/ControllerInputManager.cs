c867e8219b5e59b33271cd1b8b7035583739b33d
Nick0108 <zidonglin@outlook.com>
2017-07-23 12:12:32 +0800

    Refactor: Change the Option of selectedHand and its inspector

--- Assets/Scripts/ControllerInputManager.cs
+++ Assets/Scripts/ControllerInputManager.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class ControllerInputManager: MonoBehaviour {
 
     //Traced Controller
+    /*if for each hand
     public SteamVR_TrackedObject LeftTrackOBJ;
     public SteamVR_TrackedObject rightTrackOBJ;
-
     private SteamVR_Controller.Device leftController;
     private SteamVR_Controller.Device rightController;
+    */
+    public enum Hand
+    {
+        LeftHand,
+        RightHand
+    }
+
+    public Hand forHand;
+    private SteamVR_TrackedObject TrackObj;
+    private SteamVR_Controller.Device ControllerDevice;
 
     //Teleporter
     public GameObject TeleporterTargetObject;
@@ -20,101 +31,106 @@ public class ControllerInputManager: MonoBehaviour {
     private Vector3 TeleporterLocation;
 
     //Grabing&Throwing
+    [SerializeField,Range(1.0f,2.0f)]
     public float throwForce = 1.5f;
 
 
     // Use this for initialization
     void Start () {
+        TrackObj = GetComponent<SteamVR_TrackedObject>();
         TeleportLine = GetComponentInChildren<LineRenderer>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-            leftController = SteamVR_Controller.Input((int)LeftTrackOBJ.index);
-            rightController = SteamVR_Controller.Input((int)rightTrackOBJ.index);
-        
-        //To show the moving place
-        if (leftController.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+            ControllerDevice = SteamVR_Controller.Input((int)TrackObj.index);
+
+        //(only Lefthand)--->for Teleporting
+        if (forHand == Hand.LeftHand)
         {
-            TeleportLine.gameObject.SetActive(true);
-            TeleporterTargetObject.SetActive(true);
-            TeleportLine.SetPosition(0, transform.position);
-            RaycastHit hitInfo;
-            if(Physics.Raycast(transform.position,transform.forward,out hitInfo, 15.0f, TeleporterLayer))
+            //To show the moving place
+            if (ControllerDevice.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
             {
-                TeleporterLocation = hitInfo.transform.position;
-                TeleportLine.SetPosition(1, TeleporterLocation);
-            }
-            else
-            {
-                TeleporterLocation = transform.position + transform.forward * 15;
-                TeleportLine.SetPosition(1, transform.position + transform.forward * 15);
-                RaycastHit groundRayhit;
-                if (Physics.Raycast(TeleporterLocation, -Vector3.up,out groundRayhit, 17.0f, TeleporterLayer))
+                TeleportLine.gameObject.SetActive(true);
+                TeleporterTargetObject.SetActive(true);
+                TeleportLine.SetPosition(0, transform.position);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(transform.position, transform.forward, out hitInfo, 15.0f, TeleporterLayer))
                 {
-                    TeleporterLocation = new Vector3(transform.position.x + transform.forward.x * 15, groundRayhit.transform.position.y, transform.position.z + transform.forward.z * 15);
+                    TeleporterLocation = hitInfo.transform.position;
+                    TeleportLine.SetPosition(1, TeleporterLocation);
+                }
+                else
+                {
+                    TeleporterLocation = transform.position + transform.forward * 15;
+                    TeleportLine.SetPosition(1, transform.position + transform.forward * 15);
+                    RaycastHit groundRayhit;
+                    if (Physics.Raycast(TeleporterLocation, -Vector3.up, out groundRayhit, 17.0f, TeleporterLayer))
+                    {
+                        TeleporterLocation = new Vector3(transform.position.x + transform.forward.x * 15, groundRayhit.transform.position.y, transform.position.z + transform.forward.z * 15);
+                    }
+                }
+                TeleporterTargetObject.transform.position = TeleporterLocation + Vector3.up;
+            }
+
+            //To teleport the player
+            if (ControllerDevice.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                if (TeleporterLocation != null)
+                {
+                    Player.transform.position = TeleporterLocation;
+                    TeleportLine.gameObject.SetActive(false);
+                    TeleporterTargetObject.SetActive(false);
                 }
             }
-            TeleporterTargetObject.transform.position = TeleporterLocation + Vector3.up;
-        }
-        //To teleport the player
-        if (leftController.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
-        {
-            if (TeleporterLocation != null)
+
+            //Romove teleport
+            if (ControllerDevice.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
             {
-                Player.transform.position = TeleporterLocation;
                 TeleportLine.gameObject.SetActive(false);
                 TeleporterTargetObject.SetActive(false);
             }
         }
 
-        //Romove teleport
-        if (leftController.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
-        {
-            TeleportLine.gameObject.SetActive(false);
-            TeleporterTargetObject.SetActive(false);
-        }
-	}
-    //Grab the object
-    private void OnTriggerStay(Collider other)
-    {
-        if (leftController.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
-        {
-            GrabbingObject(LeftTrackOBJ, other);
-        }
-        else if(leftController.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
-        {
-            
-        }
-        if (rightController.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
-        {
-            GrabbingObject(rightTrackOBJ, other);
-        }
-        else if (rightController.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+        //(only RightHand) --->for menu
+        if (forHand == Hand.RightHand)
         {
 
+        }
+	}
+
+    //Grab and Throw the object
+    private void OnTriggerStay(Collider other)
+    {
+        if (ControllerDevice.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            GrabbingObject(other);
+        }
+        else if(ControllerDevice.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+        {
+            ThrowingObject(other);
         }
     }
 
-    void GrabbingObject(SteamVR_TrackedObject TraceObject, Collider GrabbingOBJ)
+    void GrabbingObject(Collider GrabbingOBJ)
     {
         if (GrabbingOBJ.CompareTag("Throwable"))
         {
-            GrabbingOBJ.transform.SetParent(TraceObject.transform);
+            GrabbingOBJ.transform.SetParent(gameObject.transform);
             GrabbingOBJ.GetComponent<Rigidbody>().isKinematic = true;
-            SteamVR_Controller.Input((int)TraceObject.index).TriggerHapticPulse(2000);
+            ControllerDevice.TriggerHapticPulse(2000);
         }
     }
 
-    void ThrowingObject(SteamVR_Controller.Device device, Collider GrabbingOBJ)
+    void ThrowingObject(Collider GrabbingOBJ)
     {
         if (GrabbingOBJ.CompareTag("Throwable"))
         {
             GrabbingOBJ.transform.SetParent(null);
             Rigidbody colliRid = GrabbingOBJ.GetComponent<Rigidbody>();
             colliRid.isKinematic = false;
-            colliRid.velocity = device.velocity * throwForce;
-            colliRid.angularVelocity = device.angularVelocity;
+            colliRid.velocity = ControllerDevice.velocity * throwForce;
+            colliRid.angularVelocity = ControllerDevice.angularVelocity;
         }
     }
 }
---
+++ Assets/Scripts/ControllerInspector.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ControllerInspector : Editor {
+
+	[CustomEditor(typeof(ControllerInputManager))]  
+    public class TestInspector : Editor  
+    {  
+        private SerializedObject obj;  
+        private ControllerInputManager controllerInputManager;  
+
+        private SerializedProperty hand;
+
+        private SerializedProperty TeleporterTargetObject;
+        private SerializedProperty Player;
+        private SerializedProperty TeleporterLayer;
+        private SerializedProperty throwForce;
+
+        void OnEnable()  
+        {  
+            obj = new SerializedObject(target);
+            TeleporterTargetObject = obj.FindProperty("TeleporterTargetObject");
+            Player = obj.FindProperty("Player");
+            TeleporterLayer = obj.FindProperty("TeleporterLayer");
+            throwForce = obj.FindProperty("throwForce");
+        }  
+      
+        public override void OnInspectorGUI()  
+        {
+            controllerInputManager = (ControllerInputManager)target;
+            controllerInputManager.forHand = (ControllerInputManager.Hand)EditorGUILayout.EnumPopup("forHand-", controllerInputManager.forHand);  
+  
+            if (controllerInputManager.forHand == ControllerInputManager.Hand.LeftHand)  
+            {  
+                EditorGUILayout.PropertyField(TeleporterTargetObject);
+                EditorGUILayout.PropertyField(Player);
+                EditorGUILayout.PropertyField(TeleporterLayer);
+            }
+            EditorGUILayout.PropertyField(throwForce);
+        }  
+    }  
+}
