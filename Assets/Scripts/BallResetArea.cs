84c62d4f827ffa7151afb1e83f7c82de4573ad79
Nick0108 <zidonglin@outlook.com>
2017-07-31 10:30:18 +0800

    Fix: fix the picked up Area

---
+++ Assets/Scripts/BallResetArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallResetArea : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Throwable"))
+        {
+            BallReset.ballThrowable = !BallReset.ballThrowable;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Throwable"))
+            BallReset.ballThrowable = !BallReset.ballThrowable;
+    }
+}
