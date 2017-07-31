5faf4512bc14324e9e43672940358e4e8da2d758
Nick0108 <zidonglin@outlook.com>
2017-07-31 10:28:37 +0800

    Fix: make the ball can be picked up

--- Assets/Scripts/BallReset.cs
+++ Assets/Scripts/BallReset.cs
@@ -7,11 +7,17 @@ public class BallReset : MonoBehaviour {
     Transform ballTransform;
     Vector3 ballPosition;
     Rigidbody ballRig;
+    
     public MyGameManager myGameManager;
+    
+    
     public static bool ballThrowable;
-
+    public static bool isOnHand;
+    //public bool isThrow = true;
 
     void Start () {
+        ballThrowable = false;
+        isOnHand = false;
         ballTransform = GetComponent<Transform>();
         ballPosition = ballTransform.position;
         ballRig = GetComponent<Rigidbody>();
@@ -25,10 +31,6 @@ public class BallReset : MonoBehaviour {
         {
             BallPosReset();
         }
-        if (collision.gameObject.CompareTag("Star")&&(gameObject.layer!=LayerMask.NameToLayer("AntiCheat")))
-        {
-            myGameManager.AddScore(collision);
-        }
         if (collision.gameObject.CompareTag("Goal"))
         {
             if (myGameManager.CheckWin())
@@ -38,30 +40,41 @@ public class BallReset : MonoBehaviour {
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("ballReleaseArea"))
-            ballThrowable = true;
-        else
-            ballThrowable = false;
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("ballReleaseArea"))
-            ballThrowable = false;
-        else
-            ballThrowable = true;
-    }
 
     //Reset the ball
     void BallPosReset()
     {
+        gameObject.transform.SetParent(null);
         gameObject.transform.position = ballPosition;
         ballRig.velocity = Vector3.zero;
         ballRig.angularVelocity = Vector3.zero;
         gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
         gameObject.layer = LayerMask.NameToLayer("Default");
         myGameManager.ScoreReset();
+        ballThrowable = false;
+        isOnHand = false;
     }
+
+    private void Update()
+    {
+        if (isOnHand)
+        {
+            if (!ballThrowable)
+            {
+                gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+                gameObject.gameObject.layer = LayerMask.NameToLayer("AntiCheat");
+            }else
+            {
+                gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+                gameObject.layer = LayerMask.NameToLayer("Default");
+            }
+        }
+
+        /*if (ballThrowable == true)
+            isThrow = true;
+        else
+            isThrow = false;*/
+    }
+
+
 }
