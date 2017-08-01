5c2a6e37260bae9d1f6e090626bcbe00835d12b1
Nick0108 <zidonglin@outlook.com>
2017-08-01 10:33:41 +0800

    Fix: make the ball can hit the star

--- Assets/Scripts/MyGameManager.cs
+++ Assets/Scripts/MyGameManager.cs
@@ -9,18 +9,34 @@ public class MyGameManager : MonoBehaviour {
     public int StarsNum;
     public GameObject gameWinCanvas;
     public int SpawnNum;
+    public SteamVR_LoadLevel loadLevel;
+
+    private GameObject currentStar;
+    private int currentStarIndex;
 
     private void Start()
     {
+        StarsNum = Stars.Count;
         currentStarNum = 0;
+        currentStarIndex = 0;
         gameWinCanvas.SetActive(false);
+        if (loadLevel == null)
+            loadLevel = GetComponent<SteamVR_LoadLevel>();
     }
 
-    public void AddScore(Collision star)
+    public void AddScore(Transform starTrans)
     {
-        Stars.Add(star.gameObject);
-        star.gameObject.SetActive(false);
+        for(int i =0;i< StarsNum; i++)
+        {
+            currentStar = Stars[i];
+            if (currentStar.transform == starTrans)
+            {
+                currentStarIndex = i;
+            }
+        }
+        Stars[currentStarIndex].SetActive(false);
         currentStarNum++;
+        //Debug.Log("currentStarNum"+currentStarNum+"==="+ "currentStarIndex" + currentStarIndex);
     }
 
     public void ScoreReset()
@@ -29,8 +45,8 @@ public class MyGameManager : MonoBehaviour {
         {
             star.SetActive(true);
         }
-        Stars = null;
         currentStarNum = 0;
+        //Debug.Log("score has reset"+currentStarNum);
     }
     public bool CheckWin()
     {
@@ -43,10 +59,18 @@ public class MyGameManager : MonoBehaviour {
     {
         gameWinCanvas.SetActive(true);
         StartCoroutine(WaitS());
-        SteamVR_LoadLevel.Begin("Level_2");
+        loadLevel.Trigger();
     }
     IEnumerator WaitS()
     {
         yield return new WaitForSeconds(2.0f);
     }
+
+    /*private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            loadLevel.Trigger();
+        }
+    }*/
 }
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour {
+
+    public MyGameManager myGameManager;
+    private Transform starTrans;
+    private void Start()
+    {
+        starTrans = GetComponent<Transform>();
+        if(myGameManager == null)
+        {
+            myGameManager = FindObjectOfType<MyGameManager>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Throwable") && other.gameObject.layer !=LayerMask.NameToLayer("AntiCheat"))
+        {
+            myGameManager.AddScore(starTrans);
+        }
+    }
+}
